e43cdfcb6e65c74448ad5f6e91ec214ad4a394ae
Goodluck Kalu <182907427+Goodluck-Kalu@users.noreply.github.com>
2026-06-27 17:58:49 +0100

    I added the ability to convert from and to currencies

--- changeAPI.js
+++ changeAPI.js
@@ -3,12 +3,16 @@ const API_KEY = "2bbace324c06543c9090f6d2";
 
 btn.addEventListener("click",async function() {
     const amount= document.getElementById("amount").value;
-    const currency = document.getElementById("currency").value;
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/NGN`);
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${fromCurrency}`);
     const data = await response.json();
-    const rates = data.conversion_rates[currency];
+
+    const rates = data.conversion_rates[toCurrency];
     const results= amount*rates;
-    document.getElementById("result").textContent=`${amount} NGN = ${results.toFixed(2)} ${currency}`;
+
+    document.getElementById("result").textContent=`${amount} ${fromCurrency} = ${results.toFixed(2)} ${toCurrency}`;
 
 }
 )
